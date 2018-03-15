c4291dc9f1beff895303f350d80be88b553e4970
okoro emeka <solomonokoro056@gmail.com>
2018-03-15 18:35:46 +0100

    added controller class for reviews

---
+++ server/controller/reviews-controller.js
@@ -0,0 +1,23 @@
+/*
+business reviews class
+*/
+class Reviews {
+  static getReviews(req, res) {
+    const Id = req.params.businessId;
+    const business = req.store.businesses[Id];
+    if (!business) {
+      res.status(404).send();
+    }
+    res.status(200).send(business.reviews);
+  }
+  static addReview(req, res) {
+    const customerReview = req.body;
+    const allCustomerReviews = req.store.businesses[req.params.businessId].reviews;
+    allCustomerReviews.push(customerReview);
+    res.status(201).send(allCustomerReviews);
+  }
+}
+
+module.exports = Reviews;
+
+
