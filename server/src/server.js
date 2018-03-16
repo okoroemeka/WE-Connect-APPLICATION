ba66dbab560136a2b85029585a50a63e3d985436
okoro emeka <solomonokoro056@gmail.com>
2018-03-16 10:50:06 +0100

    imported application router from routes folder

---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+import http from 'http';
+import router from '../Routes/app';
+
+// app.use('api/v1/', router);
+
+
+// creating the port where app will listen for incoming request
+const port = parseInt(process.env.PORT, 10) || 8000;
+
+router.set('port', port);
+
+// // creating server
+const server = http.createServer(router);
+
+server.listen(port);
