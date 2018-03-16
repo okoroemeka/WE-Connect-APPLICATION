c083fb18e8f33a57d3415181bb5006ea8f67c95a
okoro emeka <solomonokoro056@gmail.com>
2018-03-16 13:13:44 +0100

    added comments

--- server/Routes/app.js
+++ server/Routes/app.js
@@ -1,11 +1,29 @@
+// importing express router  for handling user request.
 import express from 'express';
+
+// impoerting body parser for formatting user request
+import bodyParser from 'body-parser';
+
+// import logger for returning responser
+import logger from 'morgan';
+
+// importing errorhandler taking of errors
+import errorhandler from 'errorhandler';
+
+// importing all controllers
 import signUpcontroller from '../controller/SignUp-controller';
 import loginController from '../controller/Login-controller';
 import businesscontroller from '../controller/business-controller';
 import reviewsController from '../controller/reviews-controller';
 
-const app = express.Router();
+// creating express subrouter
+const router = express();
 
+router.use(bodyParser.json());
+router.use(errorhandler());
+router.use(logger('dev'));
+
+// seed datas for testing of routes
 const store = {
   businesses: [{
     business: 'businessname',
@@ -24,20 +42,22 @@ const store = {
   LogIn: [{ username: 'name', password: 'password' }],
 };
 
-app.use((req, res, next) => {
+// making the seed datas available to controllers
+router.use((req, res, next) => {
   req.store = store;
   next();
 });
 
-app.post('/auth/signup', signUpcontroller.createNewuser);
-app.post('/auth/login', loginController.userLogIn);
-app.post('/businesses/', businesscontroller.createBusiness);
-app.put('/businesses/:businessId', businesscontroller.updateBusiness);
-app.delete('/businesses/:businessId', businesscontroller.removeBusiness);
-app.get('/businesses/:businessId', businesscontroller.getSinglebusiness);
-app.get('/businesses', businesscontroller.getBusinesses);
-app.post('/businesses/:businessId/reviews', reviewsController.addReview);
-app.get('/businesses/:businessId/reviews', reviewsController.getReviews);
+// adding end points to router
+router.post('/auth/signup', signUpcontroller.createNewuser);
+router.post('/auth/login', loginController.userLogIn);
+router.post('/businesses/', businesscontroller.createBusiness);
+router.put('/businesses/:businessId', businesscontroller.updateBusiness);
+router.delete('/businesses/:businessId', businesscontroller.removeBusiness);
+router.get('/businesses/:businessId', businesscontroller.getSinglebusiness);
+router.get('/businesses', businesscontroller.getBusinesses);
+router.post('/businesses/:businessId/reviews', reviewsController.addReview);
+router.get('/businesses/:businessId/reviews', reviewsController.getReviews);
 
-
-module.exports = app;
+// exporting the router
+module.exports = router;
