6739cd2d17b7519d1c9cda7466dd4883bb21516d
okoro emeka <solomonokoro056@gmail.com>
2018-04-15 10:09:53 +0100

    Add user signup authentication

--- server/controller/SignUp-controller.js
+++ server/controller/SignUp-controller.js
@@ -4,15 +4,15 @@ user signup class
 class Signup {
   static createNewuser(req, res) {
     // getting user information
-    const userInfo = { userName: req.body.userName, password: req.body.password };
+    const userInfo = { userName: req.body.userName, password: req.body.password , retypedPassword: req.body.retypedPassword};
     // creating the new user Id.
     const newUserId = req.store.signUp.length - 1;
     // checking if the user signup passwords match
     if (req.body.password === req.body.retypedPassword) {
       req.store.signUp.push(userInfo);
-      res.status(201).send({ newUserId });
+      return res.status(201).send({ newUserId });
     }
-    res.status(400).send({ message: " the passwords don't match " });
+    return res.status(400).send({ message: " the passwords don't match " });
   }
 }
 
