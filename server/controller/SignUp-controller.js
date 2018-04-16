5936a872e9a87edd71df382d03a5afe89004101c
okoro emeka <solomonokoro056@gmail.com>
2018-04-16 11:41:35 +0100

    Verify user data

--- server/controller/SignUp-controller.js
+++ server/controller/SignUp-controller.js
@@ -3,16 +3,29 @@ user signup class
 */
 class Signup {
   static createNewuser(req, res) {
-    // getting user information
-    const userInfo = { userName: req.body.userName, password: req.body.password , retypedPassword: req.body.retypedPassword};
+    const userName = req.body.username;
     // creating the new user Id.
-    const newUserId = req.store.signUp.length - 1;
-    // checking if the user signup passwords match
-    if (req.body.password === req.body.retypedPassword) {
-      req.store.signUp.push(userInfo);
-      return res.status(201).send({ newUserId });
+    const newUserId = req.store.signUp.length;
+    // getting user information
+    const userInfo = {
+      username: req.body.username,
+      password: req.body.password,
+      retypedPassword: req.body.retypedPassword,
+    };
+
+    // checking if username already exists
+    const usernameExist = req.store.signUp.filter(user => user.username === userName);
+    if (usernameExist.length !== 0) {
+      return res.status(400).send({
+        message: 'username already exists',
+      });
     }
-    return res.status(400).send({ message: " the passwords don't match " });
+    // checking if the user signup passwords match
+    if (req.body.password !== req.body.retypedPassword) {
+      return res.status(400).send({ message: " the passwords don't match " });
+    }
+    req.store.signUp.push(userInfo);
+    return res.status(201).send({ newUserId });
   }
 }
 
