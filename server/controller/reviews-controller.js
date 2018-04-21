7d7d7b7db913fe68c101b0f664f71bdf216105fc
okoro emeka <solomonokoro056@gmail.com>
2018-04-21 10:23:00 +0100

    Refactore code

--- server/controller/reviews-controller.js
+++ server/controller/reviews-controller.js
@@ -1,20 +1,36 @@
 /*
-business reviews class
+* business reviews class
 */
 class Reviews {
+  // Getting the reviews of business
   static getReviews(req, res) {
     const Id = req.params.businessId;
     const business = req.store.businesses[Id];
     if (!business) {
-      res.status(404).send();
+      return res.status(404).send({
+        message: 'Business not found',
+      });
     }
-    res.status(200).send(business.reviews);
+    if (business.reviews.length === 0) {
+      return res.status(404).send({
+        message: 'There are no reviews for this business',
+      });
+    }
+    return res.status(200).send(business.reviews);
   }
+
+  // Addding review to business
   static addReview(req, res) {
     const customerReview = req.body;
-    const allCustomerReviews = req.store.businesses[req.params.businessId].reviews;
-    allCustomerReviews.push(customerReview);
-    res.status(201).send(allCustomerReviews);
+    const businessToReview = req.store.businesses[req.params.businessId];
+    if (!businessToReview) {
+      return res.status(404).send({
+        message: 'Business not found',
+      });
+    }
+
+    businessToReview.reviews.push(customerReview);
+    return res.status(201).send({ message: 'comment created' });
   }
 }
 
